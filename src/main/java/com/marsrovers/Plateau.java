e2349b422084bd4ed16b702a5ed8fd80e7374dbc
willian <williancarareto@gmail.com>
2021-05-22 12:04:58 +0100

    "Created first tests for x and y coordinates"

--- src/main/java/com/marsrovers/Plateau.java
+++ src/main/java/com/marsrovers/Plateau.java
@@ -2,10 +2,12 @@ package com.marsrovers;
 
 public class Plateau {
 
+    //max size plateau on pointer x
     public static int plateauSizeX(int x) {
         return x;
     }
 
+    //max size plateau on pointer y
     public static int plateauSizeY(int y) {
         return y;
     }
--- src/test/java/com/marsrovers/PlateauTest.java
+++ src/test/java/com/marsrovers/PlateauTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class PlateauTest {
 
+    //checking max plateau size on x pointer: x max = 5
     @Test
     public void getting_plateau_size_point_x(){
         int x = 5;
@@ -12,6 +13,7 @@ public class PlateauTest {
         Assert.assertEquals(getPlateau, x);
     }
 
+    //checking max plateau size on y pointer y max = 5
     @Test
     public void getting_plateau_size_point_y(){
         int y = 5;
