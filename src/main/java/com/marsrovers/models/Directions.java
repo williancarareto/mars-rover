841157d8339beec63e81bea9c9c9e669a2ce1e1b
willian <williancarareto@gmail.com>
2021-05-23 18:28:21 +0100

    "Created failed tests for Directions"

--- src/main/java/com/marsrovers/models/Directions.java
+++ src/main/java/com/marsrovers/models/Directions.java
@@ -1,5 +1,15 @@
 package com.marsrovers.models;
 
 public enum Directions {
-    EAST, WEST, SOUTH, NORTH
+    EAST('E'), WEST('W'), SOUTH('S'), NORTH('N');
+
+    private char direction;
+
+    Directions(char direction) {
+        this.direction = direction;
+    }
+
+    public static Directions getDirection(char directionChar){
+        return null;
+    }
 }
--- src/test/java/com/marsrovers/models/DirectionsTests.java
+++ src/test/java/com/marsrovers/models/DirectionsTests.java
@@ -1,27 +1,54 @@
 package com.marsrovers.models;
 
 import com.marsrovers.exceptions.InvalidDirectionException;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DirectionsTests {
 
     @Test(expected = InvalidDirectionException.class)
     public void directions_whenLetterIsInvalid_ThrowsException() {
+        char directionChar = 'P';
+        Directions.getDirection(directionChar);
     }
 
     @Test
     public void directions_whenLetterIsN_ReturnsNorth() {
+        char directionChar = 'N';
+        Directions expected = Directions.NORTH;
+
+        Directions returnedDirection =  Directions.getDirection(directionChar);
+
+        Assert.assertEquals(expected, returnedDirection);
     }
 
     @Test
     public void directions_whenLetterIsS_ReturnsSouth() {
+        char directionChar = 'S';
+        Directions expected = Directions.SOUTH;
+
+        Directions returnedDirection =  Directions.getDirection(directionChar);
+
+        Assert.assertEquals(expected, returnedDirection);
     }
 
     @Test
     public void directions_whenLetterIsE_ReturnsEast() {
+        char directionChar = 'E';
+        Directions expected = Directions.EAST;
+
+        Directions returnedDirection =  Directions.getDirection(directionChar);
+
+        Assert.assertEquals(expected, returnedDirection);
     }
 
     @Test
     public void directions_whenLetterIsW_ReturnsWest() {
+        char directionChar = 'W';
+        Directions expected = Directions.WEST;
+
+        Directions returnedDirection =  Directions.getDirection(directionChar);
+
+        Assert.assertEquals(expected, returnedDirection);
     }
 }
