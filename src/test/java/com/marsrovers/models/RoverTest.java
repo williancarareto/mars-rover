d99a928aa26333d4311eafaa7760159499363709
willian <williancarareto@gmail.com>
2021-05-23 16:51:42 +0100

    "Created test methods on RoverTests"

---
+++ src/test/java/com/marsrovers/models/RoverTest.java
@@ -0,0 +1,31 @@
+package com.marsrovers.models;
+
+import org.junit.Test;
+
+public class RoverTest {
+
+    @Test
+    public void roverSize_WhenXIsNegative_ThrowsException(){
+
+    }
+
+    @Test
+    public void roverSize_WhenYIsNegative_ThrowsException(){
+
+    }
+
+    @Test
+    public void roverSizer_WhenXAndYAreValid_ShouldCreateRoverSuccessfully(){
+
+    }
+
+    @Test
+    public void roverActions_WhenThereIsAInvalidAction_ThrowsException(){
+
+    }
+
+    @Test
+    public void roverActions_WhenActionsAreValid_ShouldCreateRoverSuccessfully() {
+
+    }
+}
