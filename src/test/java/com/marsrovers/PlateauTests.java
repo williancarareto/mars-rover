4ee1c94d46a9797b9e67b07a1ace06e2ff5ffaac
willian <williancarareto@gmail.com>
2021-05-23 17:31:50 +0100

    "Created test methods on PlateauTests"

---
+++ src/test/java/com/marsrovers/PlateauTests.java
@@ -0,0 +1,51 @@
+package com.marsrovers;
+
+import org.junit.Test;
+
+public class PlateauTests {
+
+    @Test
+    public void plateauSize_whenXIsNegative_throwsException() {
+
+    }
+
+    @Test
+    public void plateauSize_whenYIsNegative_throwsException() {
+
+    }
+
+    @Test
+    public void plateauSize_whenXAndYAreValid_createsPlateauSuccessfully() {
+
+    }
+
+    @Test
+    public void roverPosition_whenRoverIsOutOfPlateau_ThrowsException(){
+
+    }
+
+    @Test
+    public void roverPosition_whenRoverIsInsideThePlateau_RunsSuccessfully(){
+
+    }
+
+    @Test
+    public void browseRovers_whenItHasOneRover_browseThroughThePlateauAndReturnsTheFinalPosition(){
+
+    }
+
+    @Test
+    public void browseRovers_whenItHasTwoRovers_goThroughThePlateauAndReturnsTheLastPositions(){
+
+    }
+
+    @Test
+    public void browseRovers_whenItHasTwoRoversWithCollision_ThrowsException(){
+
+    }
+
+    @Test
+    public void browseRovers_whenMoveToOutThePlateau_ThrowsException(){
+
+    }
+}
